d2b03f8c4213f0c08b6473f834c38676e9d615a5
sandao <wangwenjie002@papegames.net>
2024-06-20 21:57:46 +0800

    up

--- xhttp/response.go
+++ xhttp/response.go
@@ -1,9 +1,12 @@
 package xhttp
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+	"reflect"
 )
 
 func ResponseJson(w http.ResponseWriter, data any) {
@@ -41,10 +44,54 @@ func ResponseSSE(w http.ResponseWriter, dataChan <-chan string) {
 	}
 }
 
+func ReadRequestBody(req *http.Request) ([]byte, error) {
+	bodyBytes, err := io.ReadAll(req.Body)
+	if err != nil {
+		return nil, err
+	}
+	req.Body.Close()
+	req.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
+
+	return bodyBytes, nil
+}
+
 func DecodeBody[T any](r *http.Request) (*T, error) {
 	var v T
 	if err := json.NewDecoder(r.Body).Decode(&v); err != nil {
 		return nil, err
 	}
+	return &v, nil
+}
+
+func DecodeFormData[T any](r *http.Request) (*T, error) {
+	var v T
+
+	// Parse form data from the request
+	if err := r.ParseForm(); err != nil {
+		return nil, fmt.Errorf("error parsing form: %w", err)
+	}
+
+	// Use reflection to fill in the fields of the struct v
+	val := reflect.ValueOf(&v).Elem()
+	typ := val.Type()
+
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+		if field.CanSet() {
+			// Get the form tag for the field
+			tag := typ.Field(i).Tag.Get("form")
+			if tag == "" {
+				tag = typ.Field(i).Name // Fallback to field name
+			}
+
+			// Check if the value is provided in the form
+			if values, exists := r.Form[tag]; exists && len(values) > 0 {
+				// Assuming all fields are of string type for simplicity
+				// Convert the first value from the form to the field's type
+				field.Set(reflect.ValueOf(values[0]))
+			}
+		}
+	}
+
 	return &v, nil
 }
