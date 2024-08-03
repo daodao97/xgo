c8a92f3fab97d3e9e97449b1db22f637a4949f0c
daodao97 <daodao97@foxmail.com>
2024-08-03 09:09:14 +0800

    up

--- cache/redis_cache.go
+++ cache/redis_cache.go
@@ -8,9 +8,26 @@ import (
 	"github.com/go-redis/redis/v8"
 )
 
+type RedisOption func(c *RedisCache)
+
+func WithPrefix(prefix string) RedisOption {
+	return func(c *RedisCache) {
+		c.prefix = prefix
+	}
+}
+
 // RedisCache 是一个基于 Redis 的缓存实现
 type RedisCache struct {
 	client *redis.Client
+	prefix string
+}
+
+func NewRedisWith(options *redis.Options, option ...RedisOption) *RedisCache {
+	c := NewRedisCache(options)
+	for _, o := range option {
+		o(c)
+	}
+	return c
 }
 
 // NewRedisCache 创建一个新的 RedisCache 实例
@@ -42,9 +59,16 @@ func NewRedis(client *redis.Client) *RedisCache {
 	}
 }
 
+func (c *RedisCache) key(key string) string {
+	if c.prefix != "" {
+		return c.prefix + ":" + key
+	}
+	return key
+}
+
 // Get 从 Redis 缓存中获取指定 key 的数据
 func (c *RedisCache) Get(ctx context.Context, key string) (string, error) {
-	val, err := c.client.Get(ctx, key).Result()
+	val, err := c.client.Get(ctx, c.key(key)).Result()
 	if errors.Is(err, redis.Nil) {
 		return "", ErrNotFound
 	} else if err != nil {
@@ -55,18 +79,18 @@ func (c *RedisCache) Get(ctx context.Context, key string) (string, error) {
 
 // Del 从 Redis 缓存中删除指定 key 的数据
 func (c *RedisCache) Del(ctx context.Context, key string) error {
-	_, err := c.client.Del(ctx, key).Result()
+	_, err := c.client.Del(ctx, c.key(key)).Result()
 	return err
 }
 
 // Set 将数据存储到 Redis 缓存中，如果 key 已存在，则覆盖原有数据
 func (c *RedisCache) Set(ctx context.Context, key string, data string) error {
-	err := c.client.Set(ctx, key, data, 0).Err()
+	err := c.client.Set(ctx, c.key(key), data, 0).Err()
 	return err
 }
 
 // SetWithTTL 将数据存储到 Redis 缓存中，并设置存活时间（TTL）
 func (c *RedisCache) SetWithTTL(ctx context.Context, key string, data string, ttl time.Duration) error {
-	err := c.client.Set(ctx, key, data, ttl).Err()
+	err := c.client.Set(ctx, c.key(key), data, ttl).Err()
 	return err
 }
