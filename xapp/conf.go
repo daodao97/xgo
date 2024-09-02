f22b333bc7a2daeb073dd86f4d9f80bf603e4938
daodao97 <daodao97@foxmail.com>
2024-09-02 11:56:34 +0800

    up

---
+++ xapp/conf.go
@@ -0,0 +1,35 @@
+package xapp
+
+import (
+	"github.com/fsnotify/fsnotify"
+	"github.com/spf13/viper"
+
+	"github.com/daodao97/xgo/xlog"
+)
+
+func InitConf(dest any) error {
+	configFile := Args.Conf
+	xlog.Debug("config file", xlog.String("path", configFile))
+	viper.SetConfigType("yaml")
+	viper.SetConfigFile(configFile)
+	err := viper.ReadInConfig()
+	if err != nil {
+		return err
+	}
+	err = viper.Unmarshal(&dest)
+	if err != nil {
+		return err
+	}
+
+	viper.OnConfigChange(func(in fsnotify.Event) {
+		err := viper.Unmarshal(&dest)
+		if err != nil {
+			xlog.Error("reload config error", err)
+		}
+		xlog.Info("reload config")
+	})
+
+	viper.WatchConfig()
+
+	return nil
+}
