9a78be4c9a81b7cfef9e9025d83a13107cf634af
daodao97 <daodao97@foxmail.com>
2024-12-11 11:15:13 +0800

    up

---
+++ xutil/go.go
@@ -0,0 +1,16 @@
+package xutil
+
+import (
+	"context"
+
+	"github.com/daodao97/xgo/xlog"
+)
+
+func Go(ctx context.Context, fn func()) {
+	go func() {
+		if err := recover(); err != nil {
+			xlog.ErrorCtx(ctx, "safe go", xlog.Any("error", err))
+		}
+		fn()
+	}()
+}
