5024ec6a099e0325399a091e0bd8970b9301e2fb
daodao97 <daodao97@foxmail.com>
2025-01-07 12:02:22 +0800

    up

--- xapp/conf_test.go
+++ xapp/conf_test.go
@@ -2,16 +2,28 @@ package xapp
 
 import (
 	"fmt"
+	"os"
 	"testing"
 	"time"
+
+	"github.com/caarlos0/env/v11"
+	"github.com/daodao97/xgo/xredis"
 )
 
 func TestInitConf(t *testing.T) {
 	var conf struct {
-		Bind string `yaml:"bind" env:"BIND"`
+		Bind  string           `yaml:"bind" env:"BIND"`
+		Redis []xredis.Options `yaml:"redis" envPrefix:"REDIS"`
 	}
+	os.Setenv("BIND", "127.0.0.1:8080")
+	os.Setenv("REDIS_0_NAME", "test")
+	os.Setenv("REDIS_0_ADDR", "127.0.0.1:6379")
+	os.Setenv("REDIS_0_PASSWORD", "123456")
+	os.Setenv("REDIS_0_DB", "0")
 
-	InitConf(&conf)
+	// InitConf(&conf)
+
+	env.Parse(&conf)
 
 	fmt.Println(conf)
 	for {
--- xredis/startup.go
+++ xredis/startup.go
@@ -9,19 +9,19 @@ import (
 )
 
 type Options struct {
-	Name            string        `env:"REDIS_NAME" yaml:"name"`
-	Addr            string        `env:"REDIS_ADDR" yaml:"addr"`
-	Password        string        `env:"REDIS_PASSWORD" yaml:"password"`
-	DB              int           `env:"REDIS_DB" yaml:"db"`
-	PoolSize        int           `env:"REDIS_POOL_SIZE" yaml:"pool_size"`
-	PoolTimeout     time.Duration `env:"REDIS_POOL_TIMEOUT" yaml:"pool_timeout"`
-	ReadTimeout     time.Duration `env:"REDIS_READ_TIMEOUT" yaml:"read_timeout"`
-	WriteTimeout    time.Duration `env:"REDIS_WRITE_TIMEOUT" yaml:"write_timeout"`
-	IdleTimeout     time.Duration `env:"REDIS_IDLE_TIMEOUT" yaml:"idle_timeout"`
-	MaxRetries      int           `env:"REDIS_MAX_RETRIES" yaml:"max_retries"`
-	MinRetryBackoff time.Duration `env:"REDIS_MIN_RETRY_BACKOFF" yaml:"min_retry_backoff"`
-	MaxRetryBackoff time.Duration `env:"REDIS_MAX_RETRY_BACKOFF" yaml:"max_retry_backoff"`
-	DialTimeout     time.Duration `env:"REDIS_DIAL_TIMEOUT" yaml:"dial_timeout"`
+	Name            string        `env:"NAME" yaml:"name"`
+	Addr            string        `env:"ADDR" yaml:"addr"`
+	Password        string        `env:"PASSWORD" yaml:"password"`
+	DB              int           `env:"DB" yaml:"db"`
+	PoolSize        int           `env:"POOL_SIZE" yaml:"pool_size"`
+	PoolTimeout     time.Duration `env:"POOL_TIMEOUT" yaml:"pool_timeout"`
+	ReadTimeout     time.Duration `env:"READ_TIMEOUT" yaml:"read_timeout"`
+	WriteTimeout    time.Duration `env:"WRITE_TIMEOUT" yaml:"write_timeout"`
+	IdleTimeout     time.Duration `env:"IDLE_TIMEOUT" yaml:"idle_timeout"`
+	MaxRetries      int           `env:"MAX_RETRIES" yaml:"max_retries"`
+	MinRetryBackoff time.Duration `env:"MIN_RETRY_BACKOFF" yaml:"min_retry_backoff"`
+	MaxRetryBackoff time.Duration `env:"MAX_RETRY_BACKOFF" yaml:"max_retry_backoff"`
+	DialTimeout     time.Duration `env:"DIAL_TIMEOUT" yaml:"dial_timeout"`
 }
 
 var client *redis.Client
