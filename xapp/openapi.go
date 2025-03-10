d77f7f3777756843e03121b6415d1f732978d952
aaronhu <aaronhu@dofun.cn>
2025-03-10 14:21:16 +0800

    feat: x

--- xapp/openapi.go
+++ xapp/openapi.go
@@ -226,14 +226,16 @@ func generateSchema(t reflect.Type) Schema {
 				if jsonTag == "-" {
 					continue // 跳过被标记为忽略的字段
 				}
-				fieldName := strings.Split(jsonTag, ",")[0]
-				if fieldName == "" {
+				tag := jsonTag
+				if tag == "" {
 					formTag := field.Tag.Get("form")
 					if formTag == "-" {
 						continue
 					}
-					fieldName = strings.Split(formTag, ",")[0]
+					tag = formTag
 				}
+				fieldName := strings.Split(tag, ",")[0]
+
 				if fieldName == "" {
 					fieldName = field.Name
 				}
