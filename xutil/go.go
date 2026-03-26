292149c7d4aced49eac3772e7e4fdf30ec1fc76d
RyoJerryYu <tzl19961216@gmail.com>
2026-03-26 10:22:15 +0800

    feat(xutil/go): improve `xutil.Go` with context inherit

--- xutil/go.go
+++ xutil/go.go
@@ -22,6 +22,27 @@ func Go(ctx context.Context, fn func()) {
 	}()
 }
 
+// GoCtx is same as Go but with context
+//
+// The context passed to fn is without cancel,
+// so that it won't be canceled when the parent context is canceled.
+// It is useful for web server to avoid canceling the background task when the request is canceled.
+func GoCtx(ctx context.Context, fn func(c context.Context)) {
+	go func() {
+		defer func() {
+			if err := recover(); err != nil {
+				stack := debug.Stack()
+				xlog.ErrorCtx(ctx, "safe go",
+					xlog.Any("error", err),
+					xlog.String("stack", string(stack)),
+				)
+			}
+		}()
+		ctx = context.WithoutCancel(ctx)
+		fn(ctx)
+	}()
+}
+
 func GoWithCancel(ctx context.Context, fn func(c context.Context)) context.CancelFunc {
 	ctx, cancel := context.WithCancel(ctx)
 	go func() {
